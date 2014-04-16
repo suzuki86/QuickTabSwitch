c51bce35bfba19e8937d5c58d2af5b4ae3bbf042
suzuki86 <tsnr0001@gmail.com>
2014-04-17 01:36:34 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# QuickTabSwitch
+
+タブの切り替えを素早く行うための Google Chrome 拡張機能です。
---
+++ loadTabs.js
@@ -0,0 +1,66 @@
+window.onload = function() {
+  chrome.tabs.query({}, function(tabs){
+
+    var keies = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    var tabIds = [];
+    var keyAssigns = [];
+    var elements = '';
+    var tabsCount = tabs.length;
+    var titleLimit = '';
+    var tabElements = '';
+    var tabBlockElements = '';
+
+    if(tabsCount > 50){
+      titleLimit = 8;
+      tabElementsWidth = '190px';
+      tabBlockWidth = '160px';
+    }else if(tabsCount <= 50 && tabsCount > 34){
+      titleLimit = 16;
+      tabElementsWidth = '260px';
+      tabBlockWidth = '230px';
+    }else if(tabsCount <= 34 && tabsCount > 17){
+      titleLimit = 28;
+      tabElementsWidth = '390px';
+      tabBlockWidth = '360px';
+    }else if(tabsCount <= 17 && tabsCount > 0){
+      titleLimit = 58;
+      tabElementsWidth = '790px';
+      tabBlockWidth = '760px';
+    }
+
+    for(var i = 0; i < tabs.length; i++){
+      if(i >= keies.length){
+        break;
+      }
+      elements += '<div class="tab_element">';
+      elements += '<div class="key">' + keies[i] + '</div>';
+      elements += '<div class="tab_block">';
+      elements += '<img src="' + tabs[i].favIconUrl + '" width="16" class="favicon" />';
+      elements += '<span class="tab_title">' + tabs[i].title.substr(0, titleLimit) + '</span>';
+      elements += '</div>';
+      elements += '<div class="clear"></div>';
+      elements += '</div>';
+      keyAssigns[keies[i]] = tabs[i].id;
+    }
+    document.getElementById('tab_list').innerHTML = elements;
+
+    tabElements = document.getElementsByClassName('tab_element');
+    tabBlockElements = document.getElementsByClassName('tab_block');
+
+    for(var i = 0; i < tabs.length; i++){
+      if(i >= keies.length){
+        break;
+      }
+      tabElements[i].style.width = tabElementsWidth;
+      tabBlockElements[i].style.width = tabBlockWidth;
+    }
+
+    window.addEventListener('keydown', function(e){
+      if(e.shiftKey){
+        chrome.tabs.update(keyAssigns[String.fromCharCode(e.keyCode)], {selected: true});
+      }else{
+        chrome.tabs.update(keyAssigns[String.fromCharCode((e.keyCode)).toLowerCase()], {selected: true});
+      }
+    });
+  });
+}
